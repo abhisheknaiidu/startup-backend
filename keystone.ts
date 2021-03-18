ee5610e12bf06270bc897b6009b8173a0309988c
abhisheknaiidu <geekyabhi12@gmail.com>
2021-03-18 09:21:58 +0530

    git version updated

--- keystone.ts
+++ keystone.ts
@@ -1,4 +1,9 @@
 import 'dotenv/config';
+import { createAuth } from '@keystone-next/auth';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
 import { config, createSchema } from '@keystone-next/keystone/schema';
 import { User } from './schemas/User';
 
@@ -10,26 +15,45 @@ const sessionConfig = {
   secret: process.env.COOKIE_SECRET,
 };
 
-// little boilerplate
-export default config({
-  server: {
-    cors: {
-      origin: [process.env.FRONTEND_URL],
-      credentials: true, // meaning we're passing down the cookie, that being mentioned!
-    },
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO : Add in initial roles here
   },
-  db: {
-    adapter: 'mongoose',
-    url: databaseURL,
-    // TODO: Add data seeding here
-  },
-  lists: createSchema({
-    // scheme items go in here
-    User,
-  }),
-  ui: {
-    // TODO: change this for roles
-    isAccessAllowed: () => true,
-  },
-  // TODO: Add session values here
 });
+
+// little boilerplate
+// and wrap it with withAuth
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true, // meaning we're passing down the cookie, that being mentioned!
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+      // TODO: Add data seeding here
+    },
+    lists: createSchema({
+      // scheme items go in here
+      User,
+    }),
+    ui: {
+      // Show the UI only for people who pass this test
+      isAccessAllowed: ({ session }) =>
+        // console.log(session);
+        !!session?.data, // coercing for bool using !!
+    },
+    // Add session values here
+    session: withItemData(statelessSessions(sessionConfig), {
+      // GraphQL Query
+      User: 'id name email',
+    }),
+  })
+);
